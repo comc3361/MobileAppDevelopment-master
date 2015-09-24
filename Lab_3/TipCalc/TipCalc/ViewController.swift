d8529589e73b4357e4b216f3017d5c26bddac7c1
comc3361 <comc3361@atlas105-13.colorado.edu>
2015-09-24 10:35:56 -0600

    added variables

--- Lab_3/TipCalc/TipCalc/ViewController.swift
+++ Lab_3/TipCalc/TipCalc/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billAmount: UITextField!
+    
+    @IBOutlet weak var tipPercent: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
