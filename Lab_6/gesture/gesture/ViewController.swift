19d6c5edf42ac8ac4b12bfaa494afa1f4fc4a270
comc3361 <comc3361@atlas105-13.colorado.edu>
2015-10-13 10:46:39 -0600

    finish lab 6 - need app icons / launch screen

--- Lab_6/gesture/gesture/ViewController.swift
+++ Lab_6/gesture/gesture/ViewController.swift
@@ -7,9 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    
+    @IBAction func handlePan(sender: UIPanGestureRecognizer) {
+        let translation = sender.translationInView(view) // returns new location
+        sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
+        sender.setTranslation(CGPointZero, inView: view) //set the translation back to 0
+        
+        if sender.state == UIGestureRecognizerState.Ended { //when move ends, find velocity
+            let velocity = sender.velocityInView(self.view)
+            let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+            let slideMultiplier = magnitude / 500 //if length < 200, decrease base speed, > 200 increase base speed
+            let slideFactor = 0.1 * slideMultiplier
+            
+            var finalPoint = CGPoint(x: sender.view!.center.x + (velocity.x * slideFactor), y: sender.view!.center.y + (velocity.y * slideFactor))
+            
+            finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+            finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+            
+            UIView.animateWithDuration(Double(slideFactor * 2), delay: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: {sender.view!.center = finalPoint }, completion: nil)
+        }
+    }
+    
+    @IBAction func handlePinch(sender: UIPinchGestureRecognizer) {
+        sender.view!.transform = CGAffineTransformScale(sender.view!.transform, sender.scale, sender.scale)
+        sender.scale = 1
+    }
+    
+    @IBAction func handleRotate(sender: UIRotationGestureRecognizer) {
+        sender.view!.transform = CGAffineTransformRotate(sender.view!.transform, sender.rotation)
+        sender.rotation = 0
+    }
+    
+    var audioPlayer: AVAudioPlayer?
+    
+    @IBAction func handleLongPress(sender: UILongPressGestureRecognizer) {
+        let audioFilePath = NSBundle.mainBundle().pathForResource("Last of the Jedi", ofType: "mp3")
+        let fileURL = NSURL(fileURLWithPath: audioFilePath!)
+        audioPlayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+        if audioPlayer != nil {
+            audioPlayer!.play()
+        }
+    }
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
